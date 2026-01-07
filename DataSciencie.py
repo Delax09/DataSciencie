9cdfe30167c7febd165fbbbce52323b5de250267
Fabian Mejias <fabian.mejiast@outlook.com>
2026-01-06 23:37:24 -0300

    Mostrar el Dataset por pantalla

--- DataSciencie.py
+++ DataSciencie.py
@@ -1,2 +1,8 @@
 #Esto es para aprender y estudiar Data Science y tener algun conocimiento 
 #en el mundo laboral y/o buscar practicas de Cientifico de datos
+
+import pandas as pd
+
+dataSet = pd.read_csv('spotify-2023.csv', encoding='latin-1')
+
+print(dataSet)
